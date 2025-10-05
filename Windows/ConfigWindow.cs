0ff939c7cb07bbcb9994157feaf9661377fb936e
DJ_Shark <dj.cute.001@gmail.com>
2025-10-05 09:22:23 +0900

    コメントアウト削除

--- Windows/ConfigWindow.cs
+++ Windows/ConfigWindow.cs
@@ -7,62 +7,45 @@ namespace QuickMate.Windows;
 
 public class ConfigWindow : Window, IDisposable
 {
-	// 設定オブジェクト保持
     private readonly Configuration configuration;
 
-	//【コンストラクタ：ウィンドウ初期化】
-	// このウィンドウには、### を使用して定数 ID を割り当て
-	// これにより、ラベルを「{FPSカウンター}fps###XYZカウンターウィンドウ」のように動的にすることができる
-	// ImGuiのウィンドウIDは常に「###XYZカウンターウィンドウ」になる
     public ConfigWindow(Plugin plugin)
-        : base("QuickMate Configuration###QuickMateConfig") // ImGui ID に ### を使用
+        : base("QuickMate Configuration###QuickMateConfig")
     {
-		// ウィンドウフラグ設定
         Flags = ImGuiWindowFlags.NoResize
               | ImGuiWindowFlags.NoCollapse
               | ImGuiWindowFlags.NoScrollbar
               | ImGuiWindowFlags.NoScrollWithMouse;
 
-        // ウィンドウサイズ初期設定
         Size = new Vector2(232, 90);
         SizeCondition = ImGuiCond.Always;
 
-        // Plugin の Configuration を保持
 		configuration = plugin.Configuration;
 	}
 
-	// IDisposable 実装（必要に応じてリソース破棄）
 	public void Dispose() { }
 
-    // PreDraw(): Draw 前の処理, ウィンドウ移動可否のフラグを設定
 	public override void PreDraw()
 	{
-		// フラグはDraw()が呼び出される前に追加または削除する必要あり。そうしないと適用されない
 		if (configuration.IsConfigWindowMovable)
 		{
-			Flags &= ~ImGuiWindowFlags.NoMove; // 移動可能にする
+			Flags &= ~ImGuiWindowFlags.NoMove;
 		}
 		else
 		{
-			Flags |= ImGuiWindowFlags.NoMove; // 移動不可にする
+			Flags |= ImGuiWindowFlags.NoMove;
 		}
 	}
 
-    // Draw(): ウィンドウ描画処理
 	public override void Draw()
 	{
-		// サンプルチェックボックス：SomePropertyToBeSavedAndWithADefault
-		// プロパティを参照できないため、ローカルコピーを使用
 		var configValue = configuration.SomePropertyToBeSavedAndWithADefault;
 		if (ImGui.Checkbox("Random Config Bool", ref configValue))
 		{
-			// 値変更時に設定を更新して即保存
 			configuration.SomePropertyToBeSavedAndWithADefault = configValue;
-			//「保存して閉じる」ボタンを提供したくない場合は、変更時すぐに保存できる
 			configuration.Save();
 		}
 
-        // 設定ウィンドウを移動可能にするかのチェック
 		var movable = configuration.IsConfigWindowMovable;
 		if (ImGui.Checkbox("Movable Config Window", ref movable))
 		{
--- Windows/MainWindow.cs
+++ Windows/MainWindow.cs
@@ -10,17 +10,12 @@ namespace QuickMate.Windows;
 
 public class MainWindow : Window, IDisposable
 {
-    private readonly string ImagePath; // 表示する画像のパス
-    private readonly Plugin plugin;    // Plugin インスタンス保持
+    private readonly string ImagePath;
+    private readonly Plugin plugin;
 
-	//【コンストラクタ：ウィンドウ初期化】
-	// このウィンドウには、## を使用して隠し ID を付与
-	// ウィンドウタイトル「My Amazing Window」
-	// しかし、ImGuiの場合、IDは「My Amazing Window##With a hidden ID」
 	public MainWindow(Plugin plugin, string ImagePath)
 		: base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
 	{
-		// ウィンドウサイズ制約
 		SizeConstraints = new WindowSizeConstraints
 		{
 			MinimumSize = new Vector2(375, 330),
@@ -31,16 +26,12 @@ public class MainWindow : Window, IDisposable
 		this.plugin = plugin;
 	}
 
-	// IDisposable 実装
 	public void Dispose() { }
 
-	// Draw(): ウィンドウ描画処理
 	public override void Draw()
 	{
-		// 設定の表示
 		ImGui.TextUnformatted($"The random config bool is {plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
 
-		// 設定ウィンドウ表示ボタン
 		if (ImGui.Button("Show Settings"))
 		{
 			plugin.ToggleConfigUi();
@@ -48,13 +39,8 @@ public class MainWindow : Window, IDisposable
 
 		ImGui.Spacing();
 
-		//【子ウィンドウ（スクロール可能）を作成】
-		// 通常、BeginChild() の後には無条件の EndChild() が続く必要あり
-		// スコープ終了後はImRaiiがこれを処理
-		// これは、特定の処理を必要とするすべての ImGui 関数で機能。例としては、BeginTable() または Indent()
 		using (var child = ImRaii.Child("SomeChildWithAScrollbar", Vector2.Zero, true))
 		{
-			// 画像表示, childが絵を描いているか確認
 			if (child.Success)
 			{
 				ImGui.TextUnformatted("Have a Image:");
@@ -73,9 +59,6 @@ public class MainWindow : Window, IDisposable
 
 				ImGuiHelpers.ScaledDummy(20.0f);
 
-            	// ClientState を利用したプレイヤー情報の表示
-				// Dalamud が提供するその他のサービスの例
-				// ClientState は、ローカル プレーヤー オブジェクトとクライアントに関する情報が入ったラッパーを提供
 				var localPlayer = Plugin.ClientState.LocalPlayer;
 				if (localPlayer == null)
 				{
@@ -89,11 +72,8 @@ public class MainWindow : Window, IDisposable
 					return;
 				}
 
-				// このSeStringのマクロ表現を見たい場合は、`ToMacroString()`を使用する
 				ImGui.TextUnformatted($"Our current job is ({localPlayer.ClassJob.RowId}) \"{localPlayer.ClassJob.Value.Abbreviation}\"");
 
-				// Lumina を使った現在地域情報
-				// Lumina を直接取得、現在エリア名を取得する例
 				var territoryId = Plugin.ClientState.TerritoryType;
 				if (Plugin.DataManager.GetExcelSheet<TerritoryType>().TryGetRow(territoryId, out var territoryRow))
 				{
